9e1e81827ed7f3a1d176040a161af8f3200e1985
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-04-15 17:33:23 -0500

    creacion proyecto

---
+++ Promedio 01 - tienda/Promedio 01 - tienda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_01___tienda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
