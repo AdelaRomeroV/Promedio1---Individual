66d197f50de4d77910b72b97f9d3ac90c6313b3f
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-04-19 11:36:04 -0500

    Arreglo del codigo - Adela Romero

--- Promedio 01 - tienda/Promedio 01 - tienda/Tienda.cs
+++ Promedio 01 - tienda/Promedio 01 - tienda/Tienda.cs
@@ -8,28 +8,6 @@ namespace Promedio_01___tienda
 {
     internal class Tienda
     {
-        bool continueFlag = true;
-
-
-
-        public void ElegirOpciones()
-        {
-            while(continueFlag) 
-            {
-                Console.WriteLine("Bienvenido a la tienda, que deseas hacer:/n ");
-                Console.WriteLine("1. Contenido del carrito");
-                Console.WriteLine("2. Agregar productos");
-                Console.WriteLine("Comprar Productos");
-
-                string Option = Console.ReadLine();
-
-                switch(Option) 
-                {
-                    case "1":
-
-                        break;
-                }
-            }
-        }
+        
     }
 }
