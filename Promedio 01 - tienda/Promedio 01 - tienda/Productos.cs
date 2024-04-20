1c1f4baa8e3d45920068f2bafb7025d34367cd5a
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-04-20 16:06:36 -0500

    eliminacion de los productos - Adela Romero

--- Promedio 01 - tienda/Promedio 01 - tienda/Productos.cs
+++ Promedio 01 - tienda/Promedio 01 - tienda/Productos.cs
@@ -8,40 +8,7 @@ namespace Promedio_01___tienda
 {
     internal class Productos
     {
-        string nombres;
-        string color;
-        string material;
-        float peso;
-        float tamaño;
-        float precio;
         
-        public Arcilla(string nombre, float peso, float tamaño, string color, float precio)
-        {
-            this.nombres = nombre;
-            this.color = color;
-            this.peso = peso;
-            this.tamaño = tamaño;
-            this.precio = precio;
-        }
-
-        public Tela(string nombre, float tamaño, string material, string color, float precio)
-        {
-            this.nombres = nombre;
-            this.color = color;
-            this.tamaño = tamaño;
-            this.precio = precio;
-            this.material = material;
-        }
-
-        public void GetCarateristicas()
-        {
-
-        }
-        
-        public void GetPrecio()
-        {
-
-        }
        
     }
 }
